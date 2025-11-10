3e448993c99d1929de15c4f444fe398ec95d1392
alexk0lesnik0v <alexander74k@mail.ru>
2025-11-10 23:08:07 +0300

    Create changes

--- Assets/Scripts/ToolsController.cs
+++ Assets/Scripts/ToolsController.cs
@@ -5,17 +5,16 @@ public class ToolsController : MonoBehaviour
 {
     [SerializeField] private GameObject[] m_tools;
 
+    private GameObject m_tool;
+
     private int m_index = 0;
 
+    private MeshFilter m_mesh;
+
     private MeshFilter[] m_meshFilter;
-    /*
-    public GameObject _tools1;
-    public GameObject _tools2;
-    public GameObject _tools3;
-    public GameObject _tools4;
-    public GameObject _tools5;
-    public GameObject _tools6;
-    */
+    
+
+   //private GameObject[] m_defaultTools;
     
     private void Update()
     {
@@ -27,60 +26,45 @@ public class ToolsController : MonoBehaviour
   
     public void ChangeTools()
     {
+        m_tool = m_tools[0];
         
-        while (m_index <= m_tools.Length)
+        if (m_meshFilter == null)
         {
-            m_meshFilter[m_index] = m_tools[m_index].GetComponent<MeshFilter>();
-            m_index++;
+            m_meshFilter = new MeshFilter[m_tools.Length];
+
+            while (m_index <= m_tools.Length - 1)
+            {
+                m_mesh = m_tools[m_index].GetComponent<MeshFilter>();
+                m_meshFilter[m_index] = m_mesh;
+                m_index++;
+            }
+            m_index = 0;
+            m_mesh = m_meshFilter[0];
         }
 
-        m_index = 0;
+        
 
         while (m_index <= m_meshFilter.Length)
         {
-            if (m_index < m_meshFilter.Length)
+            if (m_index < m_meshFilter.Length - 1)
             {
+                //m_meshFilter[m_index].mesh = m_meshFilter[Random.Range(0, m_meshFilter.Length)].mesh;
                 m_meshFilter[m_index].mesh = m_meshFilter[m_index + 1].mesh;
+                m_tools[m_index] = m_tools[m_index + 1];
                 m_index++;
             }
-            else if (m_index == m_meshFilter.Length)
+            else if (m_index == m_meshFilter.Length - 1)
             {
-                m_meshFilter[m_index].mesh = m_meshFilter[0].mesh;
+                m_meshFilter[m_index].mesh = m_mesh.mesh;
+                m_tools[m_index] = m_tool;
                 m_index++;
             }
             else break;
         }
-        
-        
 
+        m_index = 0;
 
+        //m_tools = m_defaultTools;
 
-        /*
-        MeshFilter componentToCopy1 = _tools6.GetComponent<MeshFilter>();
-        MeshFilter componentToCopy2 = _tools5.GetComponent<MeshFilter>();
-        MeshFilter componentToCopy3 = _tools2.GetComponent<MeshFilter>();
-        MeshFilter componentToCopy4 = _tools1.GetComponent<MeshFilter>();
-        MeshFilter componentToCopy5 = _tools4.GetComponent<MeshFilter>();
-        MeshFilter componentToCopy6 = _tools3.GetComponent<MeshFilter>();
-
-        MeshFilter meshFilter1 = _tools1.GetComponent<MeshFilter>();
-        meshFilter1.mesh = componentToCopy1.mesh;
-        meshFilter1.mesh = componentToCopy1.mesh;
-
-        MeshFilter meshFilter6 = _tools6.GetComponent<MeshFilter>();
-        meshFilter6.mesh = componentToCopy6.mesh;
-
-        MeshFilter meshFilter3 = _tools3.GetComponent<MeshFilter>();
-        meshFilter3.mesh = componentToCopy3.mesh;
-
-        MeshFilter meshFilter2 = _tools2.GetComponent<MeshFilter>();
-        meshFilter2.mesh = componentToCopy2.mesh;
-
-        MeshFilter meshFilter5 = _tools5.GetComponent<MeshFilter>();
-        meshFilter5.mesh = componentToCopy5.mesh;
-
-        MeshFilter meshFilter4 = _tools4.GetComponent<MeshFilter>();
-        meshFilter4.mesh = componentToCopy4.mesh;
-        */
     }
 }
