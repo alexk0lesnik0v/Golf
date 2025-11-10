baa5b4df02dac0d23ec95fe88aaf8aa06520acb0
alexk0lesnik0v <alexander74k@mail.ru>
2025-11-11 01:03:32 +0300

    Add ToolsController to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private FreeCamera m_camera;
     [SerializeField] private GameObject m_uiPanel;
     [SerializeField] private CloudController m_cloudController;
+    [SerializeField] private ToolsController m_toolsController;
     private void Update()
     {
         if (m_uiPanel.activeSelf)
@@ -17,6 +18,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             m_cloudController.MoveNext();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_toolsController.ChangeTools();
         }
     }
 }
--- Assets/Scripts/ToolsController.cs
+++ Assets/Scripts/ToolsController.cs
@@ -1,4 +1,3 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class ToolsController : MonoBehaviour
@@ -13,17 +12,6 @@ public class ToolsController : MonoBehaviour
 
     private MeshFilter[] m_meshFilter;
     
-
-   //private GameObject[] m_defaultTools;
-    
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeTools();
-        }
-    }
-  
     public void ChangeTools()
     {
         m_tool = m_tools[0];
@@ -34,21 +22,17 @@ public class ToolsController : MonoBehaviour
 
             while (m_index <= m_tools.Length - 1)
             {
-                m_mesh = m_tools[m_index].GetComponent<MeshFilter>();
-                m_meshFilter[m_index] = m_mesh;
+                m_meshFilter[m_index] = m_tools[m_index].GetComponent<MeshFilter>();
                 m_index++;
             }
             m_index = 0;
             m_mesh = m_meshFilter[0];
         }
-
-        
-
+      
         while (m_index <= m_meshFilter.Length)
         {
             if (m_index < m_meshFilter.Length - 1)
             {
-                //m_meshFilter[m_index].mesh = m_meshFilter[Random.Range(0, m_meshFilter.Length)].mesh;
                 m_meshFilter[m_index].mesh = m_meshFilter[m_index + 1].mesh;
                 m_tools[m_index] = m_tools[m_index + 1];
                 m_index++;
@@ -57,14 +41,10 @@ public class ToolsController : MonoBehaviour
             {
                 m_meshFilter[m_index].mesh = m_mesh.mesh;
                 m_tools[m_index] = m_tool;
-                m_index++;
+                break;
             }
-            else break;
         }
 
         m_index = 0;
-
-        //m_tools = m_defaultTools;
-
     }
 }
